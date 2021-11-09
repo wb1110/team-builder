af77f2c44cb3997e1fd57d479f8a24d0b8b0d4a8
wb1110 <strengthcoachwb@gmail.com>
2021-11-09 12:07:46 -0600

    dropdown

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import React, {   useState  } from 'react';
 import Form from './Form';
 
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -24,24 +24,28 @@ function Form() {
     <div className="App">
       <header className="App-header">
         <form onSubmit={submit}>
-          <input 
-          value={formValues.name} 
-          name="name" 
-          type="text" 
-          onChange={change} 
-          />
-         <input 
-          value={formValues.email} 
-          name="email" 
-          type="text" 
-          onChange={change} 
-          />
-          <input 
-          value={formValues.role} 
-          name="role" 
-          type="text" 
-          onChange={change} 
-          />
+          <label>Name
+            <input 
+              value={formValues.name} 
+              name="name" 
+              type="text" 
+              onChange={change} 
+            />
+          </label>
+          <label>Email
+            <input 
+              value={formValues.email} 
+              name="email" 
+              type="text" 
+              onChange={change} 
+            />
+          </label>
+          <label>Role
+            <select value={formValues.role} name="role" onChange={change}>
+              <option value="">Select a Role</option>
+              <option value="Junior Developer">Junior Developer</option>
+            </select>
+          </label>
           <input value="Submit" type="submit"  />
           
         </form>
